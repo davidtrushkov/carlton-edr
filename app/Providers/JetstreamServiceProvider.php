01783ac0549f844b4f22233c2a6b03e02f5d2ad7
David Trushkov <davidtrushkov@users.noreply.github.com>
2021-01-07 10:47:14 -0500

    Upgrade to Jetstream v2

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -9,6 +9,7 @@ use App\Actions\Jetstream\DeleteUser;
 use App\Actions\Jetstream\UpdateTeamName;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Jetstream\Jetstream;
+use App\Actions\Jetstream\RemoveTeamMember;
 
 class JetstreamServiceProvider extends ServiceProvider
 {
@@ -36,6 +37,7 @@ class JetstreamServiceProvider extends ServiceProvider
         Jetstream::addTeamMembersUsing(AddTeamMember::class);
         Jetstream::deleteTeamsUsing(DeleteTeam::class);
         Jetstream::deleteUsersUsing(DeleteUser::class);
+        Jetstream::removeTeamMembersUsing(RemoveTeamMember::class);
     }
 
     /**
